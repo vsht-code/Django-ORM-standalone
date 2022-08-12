82cece0b747ac85ee7f48d1957f029ac2bbc923b
Vasiliy Shtykovskiy <v.sht1987@gmail.com>
2022-08-12 13:00:52 +0300

    added DB settings

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+django.setup()
+
+from datacenter.models import Passcard  # noqa: E402
+
+if __name__ == '__main__':
+    # Программируем здесь
+    print(Passcard.objects.all())
+    print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
